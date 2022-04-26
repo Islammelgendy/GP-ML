d9aef51139cb183648f671d987f0e259e4189806
islammelgendy <elgendyislam7@gmail.com>
2022-04-26 21:33:01 +0200

    prediction pipeline

--- pipeline/usage.py
+++ pipeline/usage.py
@@ -1,5 +1,12 @@
+import pickle 
 from Features import Features
 from dotenv import dotenv_values
+
+with open('models/scaler.pkl', 'rb') as f:
+  scaler = pickle.load(f)
+
+with open('models/evaluator-model.pkl', 'rb') as f:
+  model = pickle.load(f)
 
 def main():
     config = dotenv_values('.env')
@@ -11,8 +18,10 @@ def main():
     # there should be a .wav (audio) and .avi (video) in that folder and you 
     # should pass the path to the interview without an extension
     # when you test you should download the video and save it to interview folder
-    feature = features.get_features('/home/m49di/GP-ML/example/interview/P1')
-    print(feature)
+    feature = features.get_features('/home/sloaka/Desktop/GP-ML/example/interview/P1')
+    scaled_features = scaler.fit(feature)
+    print(model.predict(scaled_features))
+
 
 if __name__ == '__main__':
     main()
