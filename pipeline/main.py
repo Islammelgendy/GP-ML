42b37d81d0849840157f61c64784f988e777d26f
MuhamedMagdi <m49dii@gmail.com>
2022-05-17 02:00:43 +0200

    Configure the server

--- pipeline/main.py
+++ pipeline/main.py
@@ -1,16 +1,9 @@
 from fastapi import FastAPI
-from fastapi import FastAPI, File, UploadFile
-
 import pickle 
 from Features import Features
 from dotenv import dotenv_values
 
-
 app = FastAPI()
-
-
-
-
 
 config = dotenv_values('.env')
 apikey = config['APIKEY']
@@ -19,30 +12,41 @@ emotion_path = config['EMOTION_PATH']
 predictor_path = config['PREDICTOR_PATH']
 
 features = Features(apikey, url, emotion_path, predictor_path)
+
 with open('models/scaler.pkl', 'rb') as f:
-  scaler = pickle.load(f)
+    scaler = pickle.load(f)
 
 with open('models/evaluator-model.pkl', 'rb') as f:
-  model = pickle.load(f)
+    model = pickle.load(f)
 
 app = FastAPI()
 
+def predictionResponse(pred):
+    json = {
+        'Excited': pred[0],
+        'Engaged': pred[1],
+        'Smiled': pred[2],
+        'RecommendHiring': pred[3],
+        'NoFillers': pred[4],
+        'StructuredAnswers': pred[5],
+        'Friendly': pred[6],
+        'Focused': pred[7],
+        'NotAwkward': pred[8],
+        'Paused': pred[9],
+        'EyeContact': pred[10],
+        'Authentic': pred[11],
+        'Calm': pred[12],
+        'SpeakingRate': pred[13],
+        'NotStressed': pred[14]
+    }
+    return json
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
-
-
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
-    contents = await file.read()
-    feature = features.get_features("/home/sloaka/Desktop/GP-ML/example/interview.a/P1vi")
+@app.get("/get_features/{interviewPath:path}")
+async def getFeatures(interviewPath: str):
+    interviewPath = interviewPath.split('.')[0]
+    feature = features.get_features(interviewPath)
     normalized_features = scaler.transform(feature)
     predictions = model.predict(normalized_features)
-
-    return {"predictions":str(predictions)}
-
-
-@app.get("/file/{filePath:path}")
-def getFileByPath(filePath: str):
-    return {'received' : filePath}
+    # predictions = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15] # Used for testing the server without running the model
+    jsonPredictions = predictionResponse(predictions)
+    return {"predictions": jsonPredictions}
