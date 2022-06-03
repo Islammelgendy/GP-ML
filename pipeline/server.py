0e356248b23279e5e43abf33a1c53c7a4ad2faac
Mohamed Magdi <48320401+MuhamedMagdi@users.noreply.github.com>
2022-06-03 23:40:36 +0000

    MQ

--- pipeline/server.py
+++ pipeline/server.py
@@ -1,33 +1,23 @@
-from fastapi import FastAPI, BackgroundTasks
-from fastapi.middleware.cors import CORSMiddleware
 import pickle
+import FileProcessing
 from dotenv import dotenv_values
-import urllib.request
 import json
+import pika
 
 from Features import Features
-
-app = FastAPI()
-
-origins = [
-    "http://localhost:3000",
-    "http://localhost:80",
-    "http://localhost",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 config = dotenv_values('.env')
 apikey = config['APIKEY']
 url = config['URL']
 emotion_path = config['EMOTION_PATH']
 predictor_path = config['PREDICTOR_PATH']
+
+mq_url = config['MQ_URL']
+mq_publisher = config['MQ_PUBLISHER']
+mq_consumer = config['MQ_CONSUMER']
+
+params = pika.URLParameters(mq_url)
+connection = pika.BlockingConnection(params)
 
 features = Features(apikey, url, emotion_path, predictor_path)
 
@@ -37,47 +27,54 @@ with open('models/scaler.pkl', 'rb') as f:
 with open('models/evaluator-model.pkl', 'rb') as f:
     model = pickle.load(f)
 
-callbackURL = 'http://127.0.0.1:3000/callback'
-
 def predictionResponse(pred):
     json = {
-        'Excited': pred[0],
-        'Engaged': pred[1],
-        'Smiled': pred[2],
-        'RecommendHiring': pred[3],
-        'NoFillers': pred[4],
-        'StructuredAnswers': pred[5],
-        'Friendly': pred[6],
-        'Focused': pred[7],
-        'NotAwkward': pred[8],
-        'Paused': pred[9],
-        'EyeContact': pred[10],
-        'Authentic': pred[11],
-        'Calm': pred[12],
-        'SpeakingRate': pred[13],
-        'NotStressed': pred[14]
+        'Excited': str(pred[0][0][0]),
+        'Engaged': str(pred[1][0][0]),
+        'Smiled': str(pred[2][0][0]),
+        'RecommendHiring': str(pred[3][0][0]),
+        'NoFillers': str(pred[4][0][0]),
+        'StructuredAnswers': str(pred[5][0][0]),
+        'Friendly': str(pred[6][0][0]),
+        'Focused': str(pred[7][0][0]),
+        'NotAwkward': str(pred[8][0][0]),
+        'Paused': str(pred[9][0][0]),
+        'EyeContact': str(pred[10][0][0]),
+        'Authentic': str(pred[11][0][0]),
+        'Calm': str(pred[12][0][0]),
+        'SpeakingRate': str(pred[13][0][0]),
+        'NotStressed': str(pred[14][0][0])
     }
     return json
-    
-def callback(interviewPath, predictions):
-    jsonPredictions = predictionResponse(predictions)    
-    body = {'predictions': jsonPredictions}
-    callback = f"{callbackURL}/{interviewPath}"
-    req = urllib.request.Request(callback)
-    req.add_header('Content-Type', 'application/json; charset=utf-8')
-    jsondata = json.dumps(body)
-    jsondataasbytes = jsondata.encode('utf-8')
-    req.add_header('Content-Length', len(jsondataasbytes))
-    response = urllib.request.urlopen(req, jsondataasbytes)
+
+def publishToQueue(body):
+    channel = connection.channel()
+    channel.queue_declare(queue=mq_publisher, durable=True)
+    channel.basic_publish(exchange='',
+                            routing_key=mq_publisher,
+                            body=body)
+    channel.close()
 
 def predict(interviewPath):
+    interviewPath = interviewPath.split('.')[0]
+    FileProcessing.split_audio(interviewPath)
     feature = features.get_features(interviewPath)
     normalized_features = scaler.transform(feature)
     predictions = model.predict(normalized_features)
-    callback(interviewPath, predictions)
+    jsonPredictions = predictionResponse(predictions)
+    publishToQueue(json.dumps({"predictions": jsonPredictions}))
 
-@app.get("/get-features/{interviewPath:path}")
-async def getFeatures(interviewPath: str, background_tasks: BackgroundTasks):
-    interviewPath = interviewPath.split('.')[0]
-    background_tasks.add_task(predict, interviewPath)
-    return {"status": "processing"}
+def consuemFromQueue():
+    channel = connection.channel()
+
+    channel.queue_declare(queue=mq_consumer, durable=True)
+
+    def callback(ch, method, properties, body):
+        body = json.loads(body.decode('utf-8').replace("'", '"'))
+        predict(body['path'])
+
+    channel.basic_consume(queue=mq_consumer, on_message_callback=callback, auto_ack=True)
+
+    channel.start_consuming()
+
+consuemFromQueue()
